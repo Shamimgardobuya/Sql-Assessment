0fc0d0972e42c3e795a376fa3f1a535cffecb14e
Shamim Gard Obuya <99389344+Shamimgardobuya@users.noreply.github.com>
2022-10-12 08:54:58 +0300

    Delete question2Script.sql

--- question2Script.sql
+++
@@ -1,5 +0,0 @@
-Update analyst_intern.school_data 
-set analyst_intern.school_data.type_school_class = analyst_intern.school_data_labels.label
-where analyst_intern.school_data.type_school_class = analyst_intern.school_data_labels.code;
-
-
